4a80c9577591609d5ef096ed2e81907a646f87bc
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-04-01 19:36:29 +0200

    Create th responsive navigation bar.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import MenuView from '@/views/MenuView.vue'
+import TeamView from '@/views/TeamView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,18 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/menu',
+      name: 'menu',
+      component: MenuView
+    },
+    {
+      path: '/team',
+      name: 'team',
+      component: TeamView
+    }, {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     }
   ]
 })
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <h1>This is an home page</h1>
   </div>
 </template>
