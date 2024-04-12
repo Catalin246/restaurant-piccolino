85f46970b057a5cd7a097adf750d0781ffe44513
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-04-12 14:56:17 +0200

    Update the projects.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <h1>This is an home page</h1>
+  <div class="grid grid-rows-3 grid-flow-col gap-4 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <!-- <div class="row-auto">
+      <img src="/src/assets/pizza_home.jpeg" alt="">
+    </div> -->
+    <div class="row-auto">
+      <h1>This is an home page</h1>
+    </div>
   </div>
 </template>
 
